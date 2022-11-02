a7a0fb0e26ae20bdbcf37c05692f2ab6388b165b
henryhale <henryhale@users.noreply.github.com>
2022-11-02 11:20:23 -0400

    easily push changes

---
+++ git.bash
@@ -0,0 +1,25 @@
+#!/usr/bin/bash
+
+echo -e "\n\t [-] Git Pusher\n\n\t - starting...";
+
+echo -e "\n\t - adding...";
+
+git add .;
+
+echo -e "\n\t - current status...";
+
+git status;
+
+echo -e "\n\t - committing...";
+
+echo -ne "\n\t > ";
+
+read -p "Enter commit message: " MSG;
+
+git commit -m "$MSG";
+
+echo -e "\n\t - pushing changes...";
+
+git push;
+
+echo -e "\n\t - ...done!\n";
